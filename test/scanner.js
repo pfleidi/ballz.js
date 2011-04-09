35325c4ca7ad2cff5b74225ad271dced0b746f9f
Sven Pfleiderer <sg+github@roothausen.de>
2011-04-09 13:26:10 +0200

    Add incomplete scanner and test

---
+++ test/scanner.js
@@ -0,0 +1,34 @@
+/*!
+ * test the scanner
+ */
+
+
+var Assert = require('assert');
+var Scanner = require('../lib/scanner');
+
+var expression = '(+ (* 4 5) 4)';
+var badExpression0 = '(+ (* 4 5) 4))';
+var badExpression1 = '(+ (* 4 5) 4';
+
+
+var expectedTokens = [
+  { token: '(', type: 'L_PAREN' },
+  { token: '+', type:'ATOM' },
+  { token: ' ', type: 'WHITESPACE' },
+  { token: '(', type: 'L_PAREN' },
+  { token: '*', type:'ATOM' },
+  { token: ' ', type: 'WHITESPACE' },
+  { token: '4', type: 'NUMBER' },
+  { token: ' ', type: 'WHITESPACE' },
+  { token: '5', type: 'NUMBER' },
+  { token: ')', type: 'R_PAREN' },
+  { token: ' ', type: 'WHITESPACE' },
+  { token: '4', type: 'NUMBER' },
+  { token: ')', type: 'R_PAREN' }
+];
+
+exports.tokenize = function tokenize() {
+  var tokens = Scanner.tokenize(expression);
+  console.dir(tokens);
+  Assert.deepEqual(tokens, expectedTokens);
+};
