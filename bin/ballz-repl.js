d57ce43e957530bad6c9390bd5d38e496da8073d
Sven Pfleiderer <sg+github@roothausen.de>
2011-09-11 02:15:43 +0200

    Add basic repl

---
+++ bin/ballz-repl.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+/*!
+ * BALLZ repl module
+ *
+ * @author pfleidi
+ */
+
+var Readline = require('readline');
+var Util = require('util');
+
+var Parser = require('../lib/parser');
+var Environment = require('../lib/environment');
+var interpreter = require('../lib/interpreter').createInterpreter(
+  Environment.createEnvironment()
+);
+
+var repl = Readline.createInterface(process.stdin, process.stdout);
+
+function exec(cmd) {
+  var ast = Parser.parse(cmd);
+  return Util.inspect(interpreter.eval(ast));
+}
+
+repl.setPrompt('ballz >');
+
+repl.on('line', function (cmd) {
+    try {
+      console.log('==> ' + exec(cmd));
+      repl.prompt();
+    } catch (e) {
+      console.log(e.message);
+      console.log(e.stack);
+      repl.prompt();
+    }
+  });
+
+repl.on('SIGINT', function () {
+    repl.close();
+  });
+
+repl.on('close', function () {
+    console.log('goodbye!');
+    process.exit(0);
+  });
+
+
+repl.prompt();
