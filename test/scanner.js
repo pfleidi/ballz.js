1ad9ac225d04a35e0af3e65da7fab86bc6ad6926
Sven Pfleiderer <sg+github@roothausen.de>
2011-09-26 14:12:47 +0200

    Add scanner tests for primitive values

--- test/scanner.js
+++ test/scanner.js
@@ -8,6 +8,22 @@ var Scanner = require('../lib/scanner');
 
 Vows.describe('evaluate simple instructions').addBatch({
 
+    'scan primitive tokens': {
+
+      'parse number': function () {
+        Assert.deepEqual(Scanner.tokenize('5'), [ { type: 'NUMBER', token: '5', line: 1 } ]);
+      },
+
+      'parse symbol': function () {
+        Assert.deepEqual(Scanner.tokenize('asdf'), [ { type: 'SYMBOL', token: 'asdf', line: 1 } ]);
+      },
+
+      'parse string': function () {
+        Assert.deepEqual(Scanner.tokenize('"asdf"'), [ { type: 'STRING', token: 'asdf', line: 1 } ]);
+      }
+
+    },
+
     'test scan symbol' : function () {
       var val1 = "(define x 4)";
 
