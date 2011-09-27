f12778b34bacad2f58b998cded8dd7f3582595cd
Sven Pfleiderer <sg+github@roothausen.de>
2011-09-27 03:00:49 +0200

    Split lisp and native functions ... again

--- test/native_functions.js
+++ test/native_functions.js
@@ -1,12 +1,14 @@
 /*!
- * Unit tests for pre-defined Lisp functions
+ * Unit tests for pre-defined Functions functions
  *
  * @author pfleidi
  */
 
 var Vows = require('vows');
-var Lisp = require('../lib/lisp');
 var Assert = require('assert');
+
+var Functions = require('../lib/native_functions');
+var Lisp = require('../lib/lisp');
 
 var cons = Lisp.cons;
 var nil = Lisp.nil;
@@ -14,14 +16,14 @@ var nil = Lisp.nil;
 Vows.describe('evaluate simple instructions').addBatch({
 
     'test plus' : function () {
-      var val1 = Lisp.plus(
+      var val1 = Functions.plus(
         cons(1, cons(2, 
             cons(3, cons(4,
                 cons(5, nil() ) ) ) ) )
       );
       Assert.strictEqual(val1, 15);
 
-      var val2 = Lisp.plus(
+      var val2 = Functions.plus(
         cons(1, cons(2, 
             cons(3, cons(4,
                 cons(-5, nil() ) ) ) ) )
@@ -30,21 +32,21 @@ Vows.describe('evaluate simple instructions').addBatch({
 
       Assert.throws(
         function () {
-          var val4 = Lisp.plus(cons(1, cons('hurr', nil())));
+          var val4 = Functions.plus(cons(1, cons('hurr', nil())));
         },
         /is not a number/
       );
     }, 
 
     'test minus' : function () {
-      var val1 = Lisp.minus(
+      var val1 = Functions.minus(
         cons(1, cons(2, 
             cons(3, cons(4,
                 cons(5, nil() ) ) ) ) )
       );
       Assert.strictEqual(val1, -13);
 
-      var val2 = Lisp.minus(
+      var val2 = Functions.minus(
         cons(1, cons(2, 
             cons(3, cons(4,
                 cons(-5, nil() ) ) ) ) )
@@ -53,14 +55,14 @@ Vows.describe('evaluate simple instructions').addBatch({
     },
 
     'test multiply' : function () {
-      var val1 = Lisp.multiply(
+      var val1 = Functions.multiply(
         cons(1, cons(2, 
             cons(3, cons(4,
                 cons(5, nil() ) ) ) ) )
       );
       Assert.strictEqual(val1, 120);
 
-      var val2 = Lisp.multiply(
+      var val2 = Functions.multiply(
         cons(1, cons(2, 
             cons(3, cons(4,
                 cons(-5, nil() ) ) ) ) )
@@ -69,95 +71,95 @@ Vows.describe('evaluate simple instructions').addBatch({
     },
 
     'test divide' : function () {
-      var val1 = Lisp.divide(
+      var val1 = Functions.divide(
         cons(1, cons(2, 
             cons(3, cons(4,
                 cons(5, nil() ) ) ) ) )
       );
       Assert.strictEqual(val1, 0.008333333333333333);
 
-      var val2 = Lisp.divide(
+      var val2 = Functions.divide(
         cons(1, cons(2, 
             cons(3, cons(4,
                 cons(-5, nil() ) ) ) ) )
       );
       Assert.strictEqual(val2, -0.008333333333333333);
 
-      var val4 = Lisp.divide(cons(4, cons(0, nil())));
+      var val4 = Functions.divide(cons(4, cons(0, nil())));
       Assert.strictEqual(val4, Infinity);
     },
 
     'test equals' : function () {
-      var val1 = Lisp['equals'](cons(1, cons(2, nil())));
+      var val1 = Functions['equals'](cons(1, cons(2, nil())));
       Assert.strictEqual(val1, false);
 
-      var val2 = Lisp['equals'](cons(false, cons(false, nil())));
+      var val2 = Functions['equals'](cons(false, cons(false, nil())));
       Assert.strictEqual(val2, true);
 
-      var val3 = Lisp['equals'](cons('lala', cons('lala', nil())));
+      var val3 = Functions['equals'](cons('lala', cons('lala', nil())));
       Assert.strictEqual(val3, true);
     },
 
     'test not': function () {
-      var val1 = Lisp['not'](cons(true, nil()));
+      var val1 = Functions['not'](cons(true, nil()));
       Assert.strictEqual(val1, false);
 
-      var val2 = Lisp['not'](cons(false, nil()));
+      var val2 = Functions['not'](cons(false, nil()));
       Assert.strictEqual(val2, true);
     },
 
     'test or': function () {
-      var val1 = Lisp['or'](
+      var val1 = Functions['or'](
         cons(false, cons(false, cons(false, nil())))
       )
       Assert.strictEqual(val1, false);
 
-      var val2 = Lisp['or'](
+      var val2 = Functions['or'](
         cons(true, cons(true, cons(true, nil())))
       )
       Assert.strictEqual(val2, true);
 
-      var val3 = Lisp['or'](
+      var val3 = Functions['or'](
         cons(true, cons(true, cons(false, nil())))
       )
       Assert.strictEqual(val3, true);
 
-      var val4 = Lisp['or'](
+      var val4 = Functions['or'](
         cons(false, cons(true, cons(false, nil())))
       )
       Assert.strictEqual(val4, true);
 
-      var val5 = Lisp['or'](
+      var val5 = Functions['or'](
         cons(false, cons(false, cons(true, nil())))
       )
       Assert.strictEqual(val5, true);
     },
 
     'test and': function () {
-      var val1 = Lisp['and'](cons(true, cons(true, nil())));
+      var val1 = Functions['and'](cons(true, cons(true, nil())));
       Assert.strictEqual(val1, true);
 
-      var val2 = Lisp['and'](cons(false, cons(true, nil())));
+      var val2 = Functions['and'](cons(false, cons(true, nil())));
       Assert.strictEqual(val2, false);
 
-      var val3 = Lisp['and'](cons(true, cons(false, nil())));
+      var val3 = Functions['and'](cons(true, cons(false, nil())));
       Assert.strictEqual(val3, false);
 
-      var val4 = Lisp['and'](cons(false, cons(false, nil())));
+      var val4 = Functions['and'](cons(false, cons(false, nil())));
       Assert.strictEqual(val4, false);
     },
 
     'test xor': function () {
-      var val1 = Lisp['xor'](cons(true, cons(true, nil())));
+      var val1 = Functions['xor'](cons(true, cons(true, nil())));
       Assert.strictEqual(val1, false);
 
-      var val2 = Lisp['xor'](cons(false, cons(true, nil())));
+      var val2 = Functions['xor'](cons(false, cons(true, nil())));
       Assert.strictEqual(val2, true);
 
-      var val3 = Lisp['xor'](cons(true, cons(false, nil())));
+      var val3 = Functions['xor'](cons(true, cons(false, nil())));
       Assert.strictEqual(val3, true);
 
-      var val4 = Lisp['xor'](cons(false, cons(false, nil())));
+      var val4 = Functions['xor'](cons(false, cons(false, nil())));
       Assert.strictEqual(val4, false);
     }
 
